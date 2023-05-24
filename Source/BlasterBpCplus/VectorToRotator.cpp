a98669d9dd0da2b47eed2e5c78661433a80de91c
Mootbing <50122069+Mootbing@users.noreply.github.com>
2023-05-24 13:53:30 -0400

    init

---
+++ Source/BlasterBpCplus/VectorToRotator.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VectorToRotator.h"
+
+FRotator UVectorToRotator::VtoRotator(FVector vec)
+{
+	FRotator convers = vec.Rotation();
+	return convers;
+}
+
+float UVectorToRotator::VecSize(FVector vect)
+{
+	float Vectorr = vect.Size();
+	return Vectorr ;
+}
+
+void UVectorToRotator::ResetPlayer(AActor* Player)
+{
+	Player->Reset();
+	Player->Destroy();
+}
+
+
+
+
---
+++ Source/BlasterBpCplus/VectorToRotator.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Engine/EngineTypes.h"
+#include "GameFramework/Character.h"
+#include "TimerManager.h"
+#include "VectorToRotator.generated.h"
+
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTERBPCPLUS_API UVectorToRotator : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+		UFUNCTION(BlueprintCallable, Category = "Conversion")
+		static FRotator VtoRotator(FVector vec);
+		UFUNCTION(BlueprintCallable, Category = "Conversion")
+		static float VecSize(FVector vect);
+		UFUNCTION(BlueprintCallable, Category = "HitResultChange")
+		void SetImpactPointHit(FHitResult res, FVector impactPoint) { res.ImpactPoint = impactPoint; }
+
+		UFUNCTION(BlueprintCallable, Category = "ResetActor")
+
+		static void ResetPlayer(AActor* Player);
+
+
+};
